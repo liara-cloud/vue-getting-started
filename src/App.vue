8a7bff052c7d22ffe330c233e97bb15ce27ea99e
amirbagh75 <amirbagh75@gmail.com>
2020-06-01 14:09:09 +0430

    🎉 init project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h2 class="text">
+      Liara VueJS Deploy
+    </h2>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  background-color: #2c333f;
+}
+.container {
+  margin-top: 250px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 0px 10px;
+}
+.text {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 35px;
+  color: white;
 }
 </style>
